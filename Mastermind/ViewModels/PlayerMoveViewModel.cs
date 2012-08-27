1bd58abb311b616f8848903e1f3a55d7a381f2b4
ChrisKoenig <chris.koenig@microsoft.com>
2012-08-26 23:41:40 -0500

    enough ui - make the game stop now

--- Mastermind/ViewModels/PlayerMoveViewModel.cs
+++ Mastermind/ViewModels/PlayerMoveViewModel.cs
@@ -2,10 +2,35 @@ using GalaSoft.MvvmLight;
 using GameLogic;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
 
 namespace Mastermind.ViewModels
 {
+    public class PinViewModel : ObservableObject
+    {
+        private SolidColorBrush _PinColor;
+
+        public SolidColorBrush PinColor
+        {
+            get { return _PinColor; }
+            set
+            {
+                if (_PinColor == value)
+                    return;
+                _PinColor = value;
+                RaisePropertyChanged(() => this.PinColor);
+            }
+        }
+        
+        public PinViewModel(Color color)
+        {
+            PinColor = new SolidColorBrush(color);
+        }
+    }
+
     public class PlayerMoveViewModel : ObservableObject
     {
         // Fields...
@@ -24,6 +49,7 @@ namespace Mastermind.ViewModels
             }
         }
 
+        public ObservableCollection<PinViewModel> Results { get; set; }
 
         public GameMoveResult ResultData
         {
@@ -33,6 +59,15 @@ namespace Mastermind.ViewModels
                 if (_ResultData == value)
                     return;
                 _ResultData = value;
+                Results = new ObservableCollection<PinViewModel>();
+                for (int i = 0; i < value.NumberOfReds; i++)
+                {
+                    Results.Add(new PinViewModel(Colors.Red));
+                }
+                for (int i = 0; i < value.NumberOfWhites; i++)
+                {
+                    Results.Add(new PinViewModel(Colors.White));
+                } 
                 RaisePropertyChanged(() => this.ResultData);
             }
         }
